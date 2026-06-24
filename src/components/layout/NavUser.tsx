6d3794c0b6bad94338fc79902294e1eacc1cb638
MelikaEl <melika.melika8@gmail.com>
2026-06-24 23:46:54 +0430

    refactor imports

--- src/components/layout/NavUser.tsx
+++ src/components/layout/NavUser.tsx
@@ -2,6 +2,7 @@
 
 import { CreditCard, MoreVertical, LogOut, CircleUser } from "lucide-react";
 
+//ui components
 import { Avatar, AvatarFallback, AvatarImage } from "@/components/ui/avatar";
 import {
   DropdownMenu,
