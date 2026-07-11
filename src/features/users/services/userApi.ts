ec61f6d248af9a0c02a0629a298644a929b3633e
MelikaEl <melika.melika8@gmail.com>
2026-07-11 23:33:55 +0430

    feat: add fetch users api call

---
+++ src/features/users/services/userApi.ts
@@ -0,0 +1,37 @@
+// src/features/users/services/userApi.ts
+import { api } from "@/lib/axios";
+// import type { User, CreateUserPayload, UpdateUserPayload } from '../types';
+
+export const userApi = {
+  fetchUsers: async () => {
+    //   fetchUsers: async (): Promise<User[]> => {
+    const { data } = await api.get("/users");
+    return data;
+  },
+};
+
+// export const userApi = {
+//   fetchUsers: async (): Promise<User[]> => {
+//     const { data } = await api.get('/users');
+//     return data;
+//   },
+
+//   fetchUser: async (id: string): Promise<User> => {
+//     const { data } = await api.get(`/users/${id}`);
+//     return data;
+//   },
+
+//   createUser: async (payload: CreateUserPayload): Promise<User> => {
+//     const { data } = await api.post('/users', payload);
+//     return data;
+//   },
+
+//   updateUser: async ({ id, ...payload }: UpdateUserPayload): Promise<User> => {
+//     const { data } = await api.put(`/users/${id}`, payload);
+//     return data;
+//   },
+
+//   deleteUser: async (id: string): Promise<void> => {
+//     await api.delete(`/users/${id}`);
+//   },
+// };
