1764e1470bd1e3ff57425fc9558760a508309b89
MelikaEl <melika.melika8@gmail.com>
2026-07-11 22:47:03 +0430

    feat: enabling app to use react query

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,23 @@
 // import "./App.css";
+
+//import router
 import { RouterProvider } from "react-router-dom";
 import { router } from "./routes";
 
+//import react query
+import {
+  QueryClient,
+  QueryClientProvider,
+} from '@tanstack/react-query'
+
+const queryClient = new QueryClient()
+
 function App() {
-  return <RouterProvider router={router} />;
+  return (
+    <QueryClientProvider client={queryClient}>
+      <RouterProvider router={router} />
+    </QueryClientProvider>
+  );
 }
 
 export default App;
