eb2871303622af1f4345d0f64c478c6beb822683
MelikaEl <melika.melika8@gmail.com>
2026-07-18 12:19:07 +0430

    feat: initial users columns

---
+++ src/features/users/components/UserTableColumns.tsx
@@ -0,0 +1,7 @@
+//import tanstack
+import type { ColumnDef } from "@tanstack/react-table";
+
+//import types
+import type { GetUsersResponse } from "../types";
+
+export const userTableColumns: ColumnDef<GetUsersResponse> = [{}];
---
+++ src/features/users/components/index.ts
@@ -0,0 +1,1 @@
+export {userTableColumns} from "./UserTableColumns"
