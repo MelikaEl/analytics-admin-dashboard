4af191ea9b896e0209967439fec57b3fad931c93
MelikaEl <melika.melika8@gmail.com>
2026-07-22 12:26:31 +0430

    feat: change the table header color

--- src/components/ui/data-table.tsx
+++ src/components/ui/data-table.tsx
@@ -24,7 +24,7 @@ export function DataTable<TData, TValue>({ columns, data }: DataTableProps<TData
   return (
     <div className="overflow-hidden rounded-md border">
       <Table>
-        <TableHeader>
+        <TableHeader className="bg-gray-100">
           {table.getHeaderGroups().map((headerGroup) => (
             <TableRow key={headerGroup.id}>
               {headerGroup.headers.map((header) => {
