bf6e0c9afaf42ba72aa5db9a3e99af7171e50893
MelikaEl <melika.melika8@gmail.com>
2026-08-06 13:03:45 +0430

    feat: set pagination ui and params value to the users hook

--- src/pages/UsersPage.tsx
+++ src/pages/UsersPage.tsx
@@ -1,10 +1,79 @@
+// ui
 import { DataTable } from "@/components/ui/data-table";
+
+// users
 import { userTableColumns, useUsers } from "@/features/users";
 
-function UsersPage() {
-  const { data: users, isLoading } = useUsers();
+// react router
+import { useSearchParams } from "react-router-dom";
 
-  return <DataTable columns={userTableColumns} data={users || []} />;
+import { Field, FieldLabel } from "@/components/ui/field";
+import {
+  Pagination,
+  PaginationContent,
+  PaginationItem,
+  PaginationNext,
+  PaginationPrevious,
+} from "@/components/ui/pagination";
+import {
+  Select,
+  SelectContent,
+  SelectGroup,
+  SelectItem,
+  SelectTrigger,
+  SelectValue,
+} from "@/components/ui/select";
+
+function UsersPage() {
+  const [searchParams, setSearchParams] = useSearchParams();
+
+  const page = Number(searchParams.get("page")) || 1;
+  const limit = Number(searchParams.get("limit")) || 10;
+
+  const { data: users, isLoading } = useUsers({ page, limit });
+
+  const handlePageChange = (newPage: number) => {
+    setSearchParams({ page: String(newPage), limit: String(limit) });
+  };
+
+  const handleLimitChange = (newLimit: string) => {
+    setSearchParams({ page: "1", limit: newLimit });
+  };
+
+  return (
+    <>
+      <DataTable columns={userTableColumns} data={users || []} />
+
+      <div className="flex items-center justify-between gap-4 pt-3">
+        <Field orientation="horizontal" className="w-fit">
+          <FieldLabel htmlFor="select-rows-per-page">Rows per page</FieldLabel>
+          <Select defaultValue="25">
+            <SelectTrigger className="w-20" id="select-rows-per-page">
+              <SelectValue />
+            </SelectTrigger>
+            <SelectContent align="start">
+              <SelectGroup>
+                <SelectItem value="10">10</SelectItem>
+                <SelectItem value="25">25</SelectItem>
+                <SelectItem value="50">50</SelectItem>
+                <SelectItem value="100">100</SelectItem>
+              </SelectGroup>
+            </SelectContent>
+          </Select>
+        </Field>
+        <Pagination className="mx-0 w-auto">
+          <PaginationContent>
+            <PaginationItem>
+              <PaginationPrevious href="#" />
+            </PaginationItem>
+            <PaginationItem>
+              <PaginationNext href="#" />
+            </PaginationItem>
+          </PaginationContent>
+        </Pagination>
+      </div>
+    </>
+  );
 }
 
 export default UsersPage;
